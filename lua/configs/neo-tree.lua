bd76b457c802523fac96757551aadc7148f17b3b
Lucas Moraes <lucas.moraes88@yahoo.com>
2025-04-23 13:33:31 -0300

    feat: neo-tree sync

--- lua/configs/neo-tree.lua
+++ lua/configs/neo-tree.lua
@@ -9,6 +9,10 @@ nt.setup({
     width = 50,
   },
 	filesystem = {
+    follow_current_file = {
+      enabled = true,
+      leave_dirs_open = false,
+    },
 		filtered_items = {
 			visible = true,
 			hide_dotfiles = false,
