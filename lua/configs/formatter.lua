7efd87f27fc2fabaf8bd66c53cb2ceb7f4f03812
Lucas Moraes <lucas.moraes88@yahoo.com>
2025-04-28 00:50:34 -0300

    feat: filetype lua formatter implementation

--- lua/configs/formatter.lua
+++ lua/configs/formatter.lua
@@ -33,6 +33,21 @@ formatter.setup({
     json = { prettier_config },
     prisma = { prettier_config },
     markdown = { prettier_config },
+    lua = {
+      function()
+        return {
+          exe = "stylua",
+          args = {
+            "--search-parent-directories",
+            "--stdin-filepath",
+            vim.api.nvim_buf_get_name(0),
+            "--",
+            "-",
+          },
+          stdin = true,
+        }
+      end,
+    },
 	},
 })
 
