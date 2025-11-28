5f4d997fe6769b824cf06c54511430869c525fa2
Lucas Moraes <lsilva.actdigital@rededor.com.br>
2025-11-27 22:29:53 -0300

    chore: adjust lsp

--- lua/configs/plugins_config/lsp.lua
+++ lua/configs/plugins_config/lsp.lua
@@ -1,14 +1,32 @@
-local lspconfig = vim.lsp
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
+---@diagnostic disable: undefined-global
 
+vim.lsp.start({
+	name = "ts_ls",
+	cmd = { "typescript-language-server", "--stdio" },
+	root_dir = vim.fs.root(0, { "tsconfig.json", "package.json", ".git" }),
+})
 
--- Configuração genérica
-local servers = { "ts_ls", "cssls", "html", "lua_ls", "tailwindcss" }
+vim.lsp.start({
+	name = "cssls",
+	cmd = { "vscode-css-language-server", "--stdio" },
+	root_dir = vim.fs.root(0, { "package.json", ".git" }),
+})
 
-for _, server in ipairs(servers) do
-	vim.lsp.start({
-		name = "ts_ls",
-		cmd = { "typescript-language-server", "--stdio" },
-		root_dir = vim.fs.root(0, { "tsconfig.json", "package.json", ".git" }),
-	})
-end
+vim.lsp.start({
+	name = "html",
+	cmd = { "vscode-html-language-server", "--stdio" },
+  root_dir = vim.fs.root(0, { "package.json", ".git" }),
+})
+
+vim.lsp.start({
+	name = "lua_ls",
+	cmd = { "lua-language-server", "--stdio" },
+  root_dir = vim.fs.root(0, { ".git" }),
+})
+
+vim.lsp.start({
+  name = "tailwindcss",
+  cmd = { "tailwindcss-language-server", "--stdio" },
+  root_dir = vim.fs.root(0, { "tailwind.config.js", "package.json", ".git" }),
+})
+
