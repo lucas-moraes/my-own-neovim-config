2d672e3f009798c8c0a576f8826b3f6c8c155773
Lucas Moraes <lucas.moraes88@yahoo.com>
2025-12-02 12:18:01 -0300

    first config to python

--- lua/configs/plugins_config/lsp.lua
+++ lua/configs/plugins_config/lsp.lua
@@ -1,32 +1,40 @@
 ---@diagnostic disable: undefined-global
 
 vim.lsp.start({
-	name = "ts_ls",
-	cmd = { "typescript-language-server", "--stdio" },
-	root_dir = vim.fs.root(0, { "tsconfig.json", "package.json", ".git" }),
-})
-
-vim.lsp.start({
-	name = "cssls",
-	cmd = { "vscode-css-language-server", "--stdio" },
-	root_dir = vim.fs.root(0, { "package.json", ".git" }),
-})
-
-vim.lsp.start({
-	name = "html",
-	cmd = { "vscode-html-language-server", "--stdio" },
-  root_dir = vim.fs.root(0, { "package.json", ".git" }),
-})
-
-vim.lsp.start({
 	name = "lua_ls",
 	cmd = { "lua-language-server", "--stdio" },
   root_dir = vim.fs.root(0, { ".git" }),
 })
 
 vim.lsp.start({
-  name = "tailwindcss",
-  cmd = { "tailwindcss-language-server", "--stdio" },
-  root_dir = vim.fs.root(0, { "tailwind.config.js", "package.json", ".git" }),
+    name = "pylsp",
+    cmd = { "pylsp" },
+    root_dir = vim.fs.root(0, { ".git", "setup.py" }),
+    settings = {
+        pylsp = {
+            configurationSources = { "pycodestyle" },
+            formatCommand = "<pylsp.plugins.black>",
+            plugins = {
+                jedi_completion = { enabled = true },
+                jedi_hover = { enabled = true },
+                black = { enabled = true },
+                pylint = { enabled = true },
+                flake8 = { enabled = true },
+                pydocstyle = { enabled = false },
+            }
+        }
+    },
+    on_attach = function(client, bufnr)
+        vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, { buffer = bufnr })
+        print("pylsp conectado com sucesso!")
+    end
 })
 
+-- Instalação do LSP e dos seus plugins (Linters/Formatters)
+-- RUN pip install \
+--    pylsp \
+--    python-lsp-server[pylint] \
+--    python-lsp-server[flake8] \
+--    python-lsp-server[pydocstyle] \
+--    black \
+--    isort
