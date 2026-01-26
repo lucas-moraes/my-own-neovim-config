0cfe1eade01efc6faff0f815d0c86c37364d349f
Lucas Moraes <lucas.moraes88@yahoo.com>
2026-01-26 19:13:48 -0300

    fix: [rust] - copilot and deprecation rust-tools

--- lua/configs/main/plugins.lua
+++ lua/configs/main/plugins.lua
@@ -3,380 +3,389 @@
 -- Setup lazy.nvim
 require("lazy").setup({
 
-  "nvim-lua/plenary.nvim",
+	"nvim-lua/plenary.nvim",
 
-  -- Rust tools
-  {
-    "simrat39/rust-tools.nvim",
-    dependencies = {
-      "neovim/nvim-lspconfig",
-      "nvim-lua/plenary.nvim",
-      "mfussenegger/nvim-dap",
-    },
-    config = function()
-      require("configs.plugins_config.rust")
-    end,
-  },
+	-- Rust tools
+	{
+		"simrat39/rust-tools.nvim",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"nvim-lua/plenary.nvim",
+			"mfussenegger/nvim-dap",
+		},
+		config = function()
+			require("configs.plugins_config.rust")
+		end,
+	},
 
-  -- Crates.nvim for Cargo.toml
-  {
-    "saecki/crates.nvim",
-    event = { "BufRead Cargo.toml" },
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("crates").setup({
-        popup = {
-          autofocus = true,
-          border = "rounded",
-        },
-      })
-    end,
-  },
+	-- Crates.nvim for Cargo.toml
+	{
+		"saecki/crates.nvim",
+		event = { "BufRead Cargo.toml" },
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("crates").setup({
+				popup = {
+					autofocus = true,
+					border = "rounded",
+				},
+			})
+		end,
+	},
 
-  -- DAP (Debug Adapter Protocol)
-  {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-      "jay-babu/mason-nvim-dap.nvim",
-      "rcarriga/nvim-dap-ui",
-      "theHamsta/nvim-dap-virtual-text",
-      "nvim-neotest/nvim-nio",
-    },
-    config = function()
-      require("configs.plugins_config.dap")
-    end,
-  },
+	-- DAP (Debug Adapter Protocol)
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"jay-babu/mason-nvim-dap.nvim",
+			"rcarriga/nvim-dap-ui",
+			"theHamsta/nvim-dap-virtual-text",
+			"nvim-neotest/nvim-nio",
+		},
+		config = function()
+			require("configs.plugins_config.dap")
+		end,
+	},
 
-  -- UI helper
-  {
-    "stevearc/dressing.nvim",
-    config = function()
-      require("dressing").setup({})
-    end,
-  },
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("configs.plugins_config.copilot")
+		end,
+	},
 
-  -- Trouble (diagnostics)
-  {
-    "folke/trouble.nvim",
-    version = "v2.10.0",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-      require("trouble").setup({
-        position = "bottom",
-        height = 10,
-        icons = true,
-        mode = "document_diagnostics",
-        fold_open = "",
-        fold_closed = "",
-        action_keys = {
-          close = "q",
-          cancel = "<esc>",
-          refresh = "r",
-          jump = { "<cr>", "<tab>" },
-        },
-        auto_open = false,
-        auto_close = true,
-        signs = {
-          error = " ",
-          warning = " ",
-          hint = " ",
-          information = " ",
-        },
-      })
-    end,
-  },
+	-- UI helper
+	{
+		"stevearc/dressing.nvim",
+		config = function()
+			require("dressing").setup({})
+		end,
+	},
 
-  -- UFO - Code folding
-  {
-    "kevinhwang91/nvim-ufo",
-    dependencies = "kevinhwang91/promise-async",
-    config = function()
-      require("configs.plugins_config.nvim-ufo")
-    end,
-  },
+	-- Trouble (diagnostics)
+	{
+		"folke/trouble.nvim",
+		version = "v2.10.0",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		config = function()
+			require("trouble").setup({
+				position = "bottom",
+				height = 10,
+				icons = true,
+				mode = "document_diagnostics",
+				fold_open = "",
+				fold_closed = "",
+				action_keys = {
+					close = "q",
+					cancel = "<esc>",
+					refresh = "r",
+					jump = { "<cr>", "<tab>" },
+				},
+				auto_open = false,
+				auto_close = true,
+				signs = {
+					error = " ",
+					warning = " ",
+					hint = " ",
+					information = " ",
+				},
+			})
+		end,
+	},
 
-  -- Cursor animation
-  {
-    "sphamba/smear-cursor.nvim",
-    config = function()
-      require("smear_cursor").setup({
-        legacy_computing_symbols_support = true,
-      })
-    end,
-  },
+	-- UFO - Code folding
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = "kevinhwang91/promise-async",
+		config = function()
+			require("configs.plugins_config.nvim-ufo")
+		end,
+	},
 
-  -- Treesitter
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("configs.plugins_config.treesitter")
-    end,
-  },
+	-- Cursor animation
+	{
+		"sphamba/smear-cursor.nvim",
+		config = function()
+			require("smear_cursor").setup({
+				legacy_computing_symbols_support = true,
+			})
+		end,
+	},
 
-  -- Indent guides
-  {
-    "saghen/blink.indent",
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {},
-  },
+	-- Treesitter
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("configs.plugins_config.treesitter")
+		end,
+	},
 
-  -- Icons
-  { "nvim-tree/nvim-web-devicons" },
+	-- Indent guides
+	{
+		"saghen/blink.indent",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {},
+	},
 
-  -- Comments
-  {
-    "numToStr/Comment.nvim",
-    config = function()
-      require("Comment").setup()
-    end,
-  },
+	-- Icons
+	{ "nvim-tree/nvim-web-devicons" },
 
-  -- Formatter
-  {
-    "mhartington/formatter.nvim",
-    config = function()
-      require("configs.plugins_config.formatter")
-    end,
-  },
+	-- Comments
+	{
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	},
 
-  -- Telescope
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
+	-- Formatter
+	{
+		"mhartington/formatter.nvim",
+		config = function()
+			require("configs.plugins_config.formatter")
+		end,
+	},
 
-  -- LSP icons for completion
-  { "onsails/lspkind-nvim" },
+	-- Telescope
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 
-  -- Snippets
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "rafamadriz/friendly-snippets",
-    },
-    version = "v2.*",
-    config = function()
-      require("configs.plugins_config.luasnip")
-    end,
-  },
+	-- LSP icons for completion
+	{ "onsails/lspkind-nvim" },
 
-  -- Autocomplete
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "saadparwaiz1/cmp_luasnip",
-      "L3MON4D3/LuaSnip",
-      "rafamadriz/friendly-snippets",
-    },
-    config = function()
-      require("configs.plugins_config.cmp")
-    end,
-  },
+	-- Snippets
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+		},
+		version = "v2.*",
+		config = function()
+			require("configs.plugins_config.luasnip")
+		end,
+	},
 
-  -- Mason: Package manager for LSP/DAP
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
+	-- Autocomplete
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"saadparwaiz1/cmp_luasnip",
+			"L3MON4D3/LuaSnip",
+			"rafamadriz/friendly-snippets",
+		},
+		config = function()
+			require("configs.plugins_config.cmp")
+		end,
+	},
 
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim" },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "rust_analyzer", "lua_ls", "taplo" },
-      })
-    end,
-  },
+	-- Mason: Package manager for LSP/DAP
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-  -- File manager
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-      {
-        "s1n7ax/nvim-window-picker",
-        version = "2.*",
-        config = function()
-          require("window-picker").setup({
-            filter_rules = {
-              include_current_win = false,
-              autoselect_one = true,
-              bo = {
-                filetype = { "neo-tree", "neo-tree-popup", "notify" },
-                buftype = { "terminal", "quickfix" },
-              },
-            },
-          })
-        end,
-      },
-    },
-    config = function()
-      vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-      vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-      vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-      vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "rust_analyzer", "lua_ls", "taplo" },
+			})
+		end,
+	},
 
-      require("configs.plugins_config.neo-tree")
-    end,
-  },
+	-- File manager
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+			{
+				"s1n7ax/nvim-window-picker",
+				version = "2.*",
+				config = function()
+					require("window-picker").setup({
+						filter_rules = {
+							include_current_win = false,
+							autoselect_one = true,
+							bo = {
+								filetype = { "neo-tree", "neo-tree-popup", "notify" },
+								buftype = { "terminal", "quickfix" },
+							},
+						},
+					})
+				end,
+			},
+		},
+		config = function()
+			vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+			vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+			vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+			vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 
-  -- Color display
-  {
-    "norcalli/nvim-colorizer.lua",
-    config = function()
-      require("colorizer").setup({ "*" })
-    end,
-  },
+			require("configs.plugins_config.neo-tree")
+		end,
+	},
 
-  -- Terminal
-  {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    config = function()
-      require("configs.plugins_config.toggleterm")
-    end,
-  },
+	-- Color display
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup({ "*" })
+		end,
+	},
 
-  -- Git
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("configs.plugins_config.gitsigns")
-    end,
-  },
+	-- Terminal
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("configs.plugins_config.toggleterm")
+		end,
+	},
 
-  -- Markdown Preview
-  {
-    "iamcco/markdown-preview.nvim",
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-  },
+	-- Git
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("configs.plugins_config.gitsigns")
+		end,
+	},
 
-  -- Which-key
-  {
-    "folke/which-key.nvim",
-    config = function()
-      require("which-key")
-    end,
-  },
+	-- Markdown Preview
+	{
+		"iamcco/markdown-preview.nvim",
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
 
-  -- Mini.nvim
-  { "echasnovski/mini.nvim", version = false },
+	-- Which-key
+	{
+		"folke/which-key.nvim",
+		config = function()
+			require("which-key")
+		end,
+	},
 
-  -- Lazydocker
-  {
-    "mgierada/lazydocker.nvim",
-    dependencies = { "akinsho/toggleterm.nvim" },
-    config = function()
-      require("lazydocker").setup({})
-    end,
-  },
+	-- Mini.nvim
+	{ "echasnovski/mini.nvim", version = false },
 
-  -- Lazygit
-  {
-    "kdheepak/lazygit.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-  },
+	-- Lazydocker
+	{
+		"mgierada/lazydocker.nvim",
+		dependencies = { "akinsho/toggleterm.nvim" },
+		config = function()
+			require("lazydocker").setup({})
+		end,
+	},
 
-  -- LSP
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require("configs.plugins_config.lsp")
-    end,
-  },
+	-- Lazygit
+	{
+		"kdheepak/lazygit.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
 
-  -- Lualine
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-web-devicons" },
-  },
+	-- LSP
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.plugins_config.lsp")
+		end,
+	},
 
-  -- OpenCode
-  {
-    "NickvanDyke/opencode.nvim",
-    dependencies = {
-      { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
-    },
-    config = function()
-      require("configs.plugins_config.opencodeconfig")
-    end,
-  },
+	-- Lualine
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-web-devicons" },
+	},
 
-  -- Transparent background
-  {
-    "xiyaowong/nvim-transparent",
-    config = function()
-      require("transparent").setup({
-        groups = {
-          "Normal",
-          "NormalNC",
-          "Special",
-          "Identifier",
-          "Statement",
-          "PreProc",
-          "Type",
-          "Underlined",
-          "Todo",
-          "String",
-          "Function",
-          "Conditional",
-          "Repeat",
-          "Operator",
-          "Structure",
-          "LineNr",
-          "NonText",
-          "SignColumn",
-          "CursorLineNr",
-          "EndOfBuffer",
-        },
-        extra_groups = {},
-        exclude_groups = {},
-      })
-    end,
-  },
+	-- OpenCode
+	{
+		"NickvanDyke/opencode.nvim",
+		dependencies = {
+			{ "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+		},
+		config = function()
+			require("configs.plugins_config.opencodeconfig")
+		end,
+	},
+
+	-- Transparent background
+	{
+		"xiyaowong/nvim-transparent",
+		config = function()
+			require("transparent").setup({
+				groups = {
+					"Normal",
+					"NormalNC",
+					"Special",
+					"Identifier",
+					"Statement",
+					"PreProc",
+					"Type",
+					"Underlined",
+					"Todo",
+					"String",
+					"Function",
+					"Conditional",
+					"Repeat",
+					"Operator",
+					"Structure",
+					"LineNr",
+					"NonText",
+					"SignColumn",
+					"CursorLineNr",
+					"EndOfBuffer",
+				},
+				extra_groups = {},
+				exclude_groups = {},
+			})
+		end,
+	},
 }, {
-  -- Lazy.nvim configuration options
-  ui = {
-    icons = {
-      cmd = "⌘",
-      config = "🛠",
-      event = "📅",
-      ft = "📂",
-      init = "⚙",
-      keys = "🗝",
-      plugin = "🔌",
-      runtime = "💻",
-      require = "🌙",
-      source = "📄",
-      start = "🚀",
-      task = "📌",
-    },
-  },
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        "gzip",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
-    },
-  },
+	-- Lazy.nvim configuration options
+	ui = {
+		icons = {
+			cmd = "⌘",
+			config = "🛠",
+			event = "📅",
+			ft = "📂",
+			init = "⚙",
+			keys = "🗝",
+			plugin = "🔌",
+			runtime = "💻",
+			require = "🌙",
+			source = "📄",
+			start = "🚀",
+			task = "📌",
+		},
+	},
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
 })
 
 require("configs.plugins_config.theme-manager")
--- lua/configs/plugins_config/lsp.lua
+++ lua/configs/plugins_config/lsp.lua
@@ -1,62 +1,63 @@
-local status, nvim_lsp = pcall(require, "lspconfig")
-if not status then
-  return
+-- LSP configuration for nvim-lspconfig v3.0.0+
+-- Uses vim.lsp.config API
+
+local cmp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if not cmp_status then
+	return
 end
 
-local on_attach = function(client, bufnr)
-  local opts = { noremap = true, silent = true, buffer = bufnr }
+local capabilities = cmp_nvim_lsp.default_capabilities()
 
-  -- LSP keybindings
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-  vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-  vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
-  vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-  vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+-- Global LSP keybindings via LspAttach autocmd
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
+	callback = function(ev)
+		local opts = { noremap = true, silent = true, buffer = ev.buf }
 
-  -- Format on save
-  if client.server_capabilities.documentFormattingProvider then
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = vim.api.nvim_create_augroup("LspFormat", { clear = true }),
-      buffer = bufnr,
-      callback = function()
-        vim.lsp.buf.format({ bufnr = bufnr })
-      end,
-    })
-  end
-end
-
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
--- Lua (for Neovim config)
-nvim_lsp.lua_ls.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      runtime = {
-        version = "LuaJIT",
-      },
-      diagnostics = {
-        globals = { "vim" },
-      },
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-        checkThirdParty = false,
-      },
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+		vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+	end,
 })
 
--- TOML (for Cargo.toml)
-nvim_lsp.taplo.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-})
+-- Lua LS (for Neovim config)
+vim.lsp.config.lua_ls = {
+	cmd = { "lua-language-server" },
+	filetypes = { "lua" },
+	root_markers = { ".luarc.json", ".luarc.jsonc", ".luacheckrc", ".stylua.toml", "stylua.toml", "selene.toml", "selene.yml", ".git" },
+	capabilities = capabilities,
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false,
+			},
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+}
+
+-- Taplo (for TOML/Cargo.toml)
+vim.lsp.config.taplo = {
+	cmd = { "taplo", "lsp", "stdio" },
+	filetypes = { "toml" },
+	root_markers = { ".taplo.toml", "taplo.toml", ".git" },
+	capabilities = capabilities,
+}
+
+-- Enable the configured servers
+vim.lsp.enable({ "lua_ls", "taplo" })
